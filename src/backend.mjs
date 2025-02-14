03efd07f6262467e658fe2c04ab36f875f99c3ba
KOCAOGLU Manolia <157006573+manolia-kocaoglu@users.noreply.github.com>
2025-02-14 14:13:59 +0100

    Add functionality to create new offers; implement form for adding house details in the offres page

--- src/backend.mjs
+++ src/backend.mjs
@@ -80,3 +80,20 @@ export async function bySurface(lasurface) {
     return [];
     }
   }
+
+
+  export async function addOffre(house) {
+    try {
+        await pb.collection('Agence').create(house);
+        return {
+            success: true,
+            message: 'Offre ajoutée avec succès'
+        };
+    } catch (error) {
+        console.log('Une erreur est survenue en ajoutant la maison', error);
+        return {
+            success: false,
+            message: 'Une erreur est survenue en ajoutant la maison'
+        };
+    }
+}
