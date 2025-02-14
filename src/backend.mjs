4e77cc5d5afc7bf18ea4c810da85cd87a1c79718
KOCAOGLU Manolia <157006573+manolia-kocaoglu@users.noreply.github.com>
2025-02-14 14:26:10 +0100

    filtrage par prix

--- src/backend.mjs
+++ src/backend.mjs
@@ -97,3 +97,20 @@ export async function bySurface(lasurface) {
         };
     }
 }
+
+export async function filterByPrix(prixMin, prixMax) {
+    try {
+        let data = await pb.collection('Agence').getFullList({
+            sort: '-created',
+            filter: `prix >= ${prixMin} && prix <= ${prixMax}`
+        });
+        data = data.map((maison) => {
+            maison.imageUrl = pb.files.getURL(maison, maison.image);
+            return maison;
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en filtrant la liste des maisons', error);
+        return [];
+    }
+}
